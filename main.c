9cf674f002c17aa0803d78ce66519d417ab51374
Lucas Black <hmnotsocool@gmail.com>
2019-12-03 02:29:40 -0800

    working CLI natural numbers calculator

--- main.c
+++ main.c
@@ -1,19 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// what we want:
+int check_operator(char *op)
+{
+    enum operators{
+        add,
+        sub,
+        mult,
+        div,
+        unknown
+    };
 
-// - addition (a + b = c)
-// - subtraction (a - b = c)
-// - multiplication (a * b = c)
-// - division (a / b = c)
+    if (*op == '+') {
+        return add;
+    } else if (*op == '-') {
+        return sub;
+    } else if (*op == '.') {
+        return mult;
+    } else if (*op == '/') {
+        return div;
+    }
 
-// step 1:
-// - implement the functions above
+    return unknown;
+}
 
-// step 2:
-// - implement order of operations and some algebraic functions
+void output_result(int a, int b, int op)
+{
+    if (op == 0)
+        printf("%d + %d = %d\n", a, b, a + b);
+    else if (op == 1)
+        printf("%d - %d = %d\n", a, b, a - b);
+    else if (op == 2)
+        printf("%d . %d = %d\n", a, b, a * b);
+    else if (op == 3)
+        printf("%d / %d = %d\n", a, b, a / b);
+    else {
+        printf("error: unknown operator\n");
+    }
+}
 
 int main(int argc, char **argv)
 {
+    if (argc < 4) {
+        printf("error: use with `calculate (int) (operator) (int)`\n");
+        exit(1);
+    }
+
+    int a, b;
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+
+    int current_operation = check_operator(argv[2]);
+    output_result(a, b, current_operation);
+
     return 0;
 }
