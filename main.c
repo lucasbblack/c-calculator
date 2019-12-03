6cd24c740980e6aa1ab4eaf534079ac542f5f917
Lucas Black <lucasbblack@gmail.com>
2019-12-02 21:59:58 -0800

    init commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// what we want:
+
+// - addition (a + b = c)
+// - subtraction (a - b = c)
+// - multiplication (a * b = c)
+// - division (a / b = c)
+
+// step 1:
+// - implement the functions above
+
+// step 2:
+// - implement order of operations and some algebraic functions
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
